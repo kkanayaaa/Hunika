2a0768a82f23861b1e9337f6afc47e9132f5f06e
kkanayaaa <kanayaanantanisyafikri@gmail.com>
2026-06-28 18:22:50 +0700

    implementasi responsive review and rating page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import ReviewKos from './pages/Review/ReviewKos';
+
+
+const App = () => {
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <ReviewKos />
+    </div>
+  );
+}
+
+export default App;
